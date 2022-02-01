e8cade9814909b26dde9bf0f294629e4463fd799
Mads Færch <mads.faerch@gmail.com>
2022-02-01 23:15:10 +0100

    Add critters and font to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+          @font-face {
+            font-family: Akronim;
+            src: url("/akronim.woff2");
+          }
+        `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
